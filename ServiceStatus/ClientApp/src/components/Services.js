272c558de6bed0c41acf766913309172c8c9f2c7
josecarlos55 <46457623+josecarlos55@users.noreply.github.com>
2021-05-03 20:45:45 +0100

    Update buttons funcionando

--- ServiceStatus/ClientApp/src/components/Services.js
+++ ServiceStatus/ClientApp/src/components/Services.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export class Services extends Component {
     static displayName = Services.name;
-
+    
+  
     constructor(props) {
         super(props);
         this.state = { forecasts: [], loading: true };
@@ -13,49 +17,53 @@ export class Services extends Component {
                 this.setState({ forecasts: data, loading: false });
             });
     }
+     onSubmit () {
+      
+       const history = useHistory();
+       history.push('/Historic');
+    }
+
 
     incrementCounter() {
-        this.setState({
-            currentCount: this.state.currentCount + 1
-        });
+        console.log("Teste")
+       
     }
 
-    static renderForecastsTable(forecasts) {
-        return (
-            <table className='table table-striped'>
-                <thead>
-                    
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Estado</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {forecasts.map(forecast => {
-                        return <tr key={forecast.Id}>
-                            <td>{forecast.Id}</td>
-                            <td>{forecast.Nome}</td>
-                            <td>{forecast.Estado}</td>
-
-                            <button className="btn btn-primary" onClick={this.incrementCounter}> Historic </button>
-                            <button className="btn btn-primary" onClick={this.incrementCounter}> Subscribe </button>
-                            
-                            
-                        </tr>
-                    })}
-                </tbody>
-            </table>
-        );
-    }
+   
 
     render() {
-        let contents = Services.renderForecastsTable(this.state.forecasts);
+        //let contents = this.renderForecastsTable(this.state.forecasts);
 
         return (
             <div>
-                {contents}
+                <table className='table table-striped'>
+                    <thead>
+
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Estado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.forecasts.map(forecast => {
+                            return <tr key={forecast.Id}>
+                                <td>{forecast.Id}</td>
+                                <td>{forecast.Nome}</td>
+                                <td>{forecast.Estado}</td>
+                              
+                                <td><NavLink exact activeClassName="active" to="/Historic"> Historic </NavLink></td>
+
+                                <td><button className="btn btn-primary" onClick={this.incrementCounter}> Subscribe </button></td>
+
+
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
             </div>  
         );
     }
+
 }
+
